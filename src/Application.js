3be2223de7a929986b9cd080a45c3eb94edcde50
Hunter Morgenstern <hunter.morgenstern@gmail.com>
2018-08-16 23:59:53 -0700

    deployed on aws

--- src/Application.js
+++ src/Application.js
@@ -1,31 +1,42 @@
-import React, { Component } from 'react';
-import NewGrudge from './NewGrudge';
-import Grudges from './Grudges';
-import './Application.css';
+import React, { Component } from "react";
+import NewGrudge from "./NewGrudge";
+import Grudges from "./Grudges";
+import "./Application.css";
 
-import { withAuthenticator } from 'aws-amplify-react';
+import { API } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react";
 
 class Application extends Component {
   state = {
-    grudges: [],
+    grudges: []
   };
 
+  componentDidMount() {
+    API.get("grudgesCRUD", "/grudges").then(grudges => {
+      this.setState({ grudges });
+    });
+  }
+
   addGrudge = grudge => {
-    this.setState({ grudges: [grudge, ...this.state.grudges] });
+    API.post("grudgesCRUD", "/grudges", { body: grudge }).then(() => {
+      this.setState({ grudges: [grudge, ...this.state.grudges] });
+    });
   };
 
   removeGrudge = grudge => {
-    this.setState({
-      grudges: this.state.grudges.filter(other => other.id !== grudge.id),
+    API.del("grudgesCRUD", "/grudges/object/" + grudge.id).then(() => {
+      this.setState({
+        grudges: this.state.grudges.filter(other => other.id !== grudge.id)
+      });
     });
   };
 
   toggle = grudge => {
-    const othergrudges = this.state.grudges.filter(
-      other => other.id !== grudge.id,
-    );
     const updatedGrudge = { ...grudge, avenged: !grudge.avenged };
-    this.setState({ grudges: [updatedGrudge, ...othergrudges] });
+    API.put("grudgesCRUD", "/grudges", { body: updatedGrudge }).then(() => {
+      const othergrudges = this.state.grudges.filter(other => other.id !== grudge.id);
+      this.setState({ grudges: [updatedGrudge, ...othergrudges] });
+    });
   };
 
   render() {
